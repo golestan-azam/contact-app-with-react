9d37862cc91223db1c2202e46c2fa8c98f47bd13
azam golestan <goli.goli499a.com>
2024-01-25 00:21:24 +0330

    Created Header Component

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import styles from "./Header.module.css"
+function Header() {
+    return (
+        <div className={styles.container}>
+            <h1>Contact App</h1>
+            <p>
+                <a href=""></a>
+            </p>
+        </div>
+    );
+}
+
+export default Header;
