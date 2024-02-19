94715013b32ef812857d8336e31f43aa05d68a0f
azam golestan <goli.goli499a.com>
2024-02-19 10:50:07 +0330

    Created Contacts Component and Created form

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
+import Contacts from "./components/Contacts"
 import Header from "./components/Header"
 
 function App() {
 
   return (
     <>
-      <Header />    
+      <Header />  
+      <Contacts />  
     </>
   )
 }
